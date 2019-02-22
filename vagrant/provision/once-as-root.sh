f0cfdb4c844ae7bf7a08a47274c0f6dfa9d2ba59
Sergey Navruzov <navruzov@infosystems.uz>
2019-02-22 16:02:02 +0500

    redis-server

--- vagrant/provision/once-as-root.sh
+++ vagrant/provision/once-as-root.sh
@@ -28,7 +28,7 @@ apt-get update
 apt-get upgrade -y
 
 info "Install additional software"
-apt-get install -y php7.0-curl php7.0-cli php7.0-intl php7.0-mysqlnd php7.0-gd php7.0-fpm php7.0-mbstring php7.0-xml unzip nginx mysql-server-5.7 php.xdebug
+apt-get install -y php7.0-curl php7.0-cli php7.0-intl php7.0-mysqlnd php7.0-gd php7.0-fpm php7.0-mbstring php7.0-xml unzip nginx mysql-server-5.7 php.xdebug redis-server
 
 info "Configure MySQL"
 sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
